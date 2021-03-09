8bc7ae0d217e52f3ae925938c20f0c919a85eaeb
Khaleel Ahamed <omdkhaleel@gmail.com>
2021-03-09 15:31:19 +0530

    Finding GCD using list & append

---
+++ gcd-old.py
@@ -0,0 +1,16 @@
+# Finding greatest common divisor (gcd) using list.
+def gcd(m,n):
+	fm=[]
+	for i in range (1,m+1):
+		if(m%i)==0:
+			fm.append(i)
+	fn=[]
+	for j in range (1,n+1):
+		if(n%j)==0:
+			fn.append(j)
+	cf=[]
+	for f in fm:
+		if f in fn:
+			cf.append(f)
+			print(f)
+	return(cf[-1])
