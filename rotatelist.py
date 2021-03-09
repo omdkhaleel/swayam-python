c32361e0dffe92210bb763a32d3d840af2d6739e
Khaleel Ahamed <omdkhaleel@gmail.com>
2021-03-09 15:44:36 +0530

    Rotating list element for k times

--- rotatelist.py
+++ rotatelist.py
@@ -3,3 +3,14 @@ def rotatelist(l,k):
     l=l[1:len(l)]+[l[0]]
     k=k-1
   return(l)
+
+# One more way
+def rotatelist(l,k):
+  retlist = l[:]
+
+  if k <= 0:
+    return(retlist)
+
+  for i in range(1,k+1):
+    retlist = retlist[1:] + [retlist[0]]
+  return(retlist)
