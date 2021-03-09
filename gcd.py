3fe2207f33fcb87dbd8a2acad57bf9719e7f1e0d
Khaleel Ahamed <omdkhaleel@gmail.com>
2021-03-09 15:32:54 +0530

    Delete gcd.py

--- gcd.py
+++
@@ -1,8 +0,0 @@
-# Find out greatest common divisor(gcd) of given two numbers.
-def gcd(m,n):
-    if m < n:
-        (m,n)=(n,m)
-    while (m%n)!= 0:
-        (m,n)=(n,m%n)
-        print(m,n)
-    return(n)
