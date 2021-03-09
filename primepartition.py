b2e9ca251890bf70c99be3bd1a44c115dc5ee088
Khaleel Ahamed <omdkhaleel@gmail.com>
2021-03-09 15:42:09 +0530

    Partitioning an integer number into prime numbers

--- primepartition.py
+++ primepartition.py
@@ -14,3 +14,20 @@ def primepartition(m):
         if isprime(m-i):
           primelist = primelist + [i]+[m-i]
     return(primelist!=[])
+
+# One more way
+def factors(n):
+  factorlist = []
+  for i in range(1,n+1):
+    if n%i == 0:
+      factorlist.append(i)
+  return(factorlist)
+
+def prime(n):
+  return(factors(n)==[1,n])
+
+def primepartition(n):
+  for i in range(1,n//2+1):
+    if prime(i) and prime(n-i):
+      return(True)
+  return(False)
