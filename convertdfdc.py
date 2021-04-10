8401ea25cf68769d708feb4e2ce9a3f15d072736
Khaleel Ahamed <omdkhaleel@gmail.com>
2021-04-10 08:20:26 +0530

    Convert temperature from Fahrenheit to Celsius

--- convertdfdc.py
+++ convertdfdc.py
@@ -1,5 +1,8 @@
 def dconvert():
-  inp = input("Enter Temperature in 'F: ")
-  F = float(inp)
-  C = (F-32)*5/9
-  print(C)
+  try:
+    inp = input("Enter Temperature in 'F: ")
+    F = float(inp)
+    C = (F-32)*5/9
+    print(C)
+  except:
+    print("Please enter valid temperature")
